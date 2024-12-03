dbc4df1b30151cde144c29b2c6533b3b2570b413
Shadowz <sh4dowswat@gmail.com>
2024-12-03 12:50:30 +0100

    Commit 2, Added semi-routes (to-do)

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,60 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {FeaturesComponent} from './features/features.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: '/home',
+    component: HomeComponent,
+  },
+  {
+    path: '/about',
+    component: AboutComponent,
+  },
+  {
+    path: '/contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'services',
+    component: FeaturesComponent
+  },
+  {
+    path: 'gestionale',
+    pathMatch: 'prefix',
+    children: [
+      {
+        path: 'dashboard'
+      },
+      {
+        path: 'items'
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path : 'login',
+      },
+      {
+        path : 'register',
+      },
+      {
+        path: 'customize',
+      }
+    ]
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  },
+];
