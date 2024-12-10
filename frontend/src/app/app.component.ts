f9980a2bdc1c2f7c7ad88b2837aa841989e3badf
Giovanni Muraca <ghadprogrammer@gmail.com>
2024-12-10 02:47:21 +0100

    Aggiunto componente per la mappa.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
+import {MapComponent} from './map/map.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
