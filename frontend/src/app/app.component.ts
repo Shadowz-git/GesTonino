eb9cb14a035a5cd1a0ac1ed30c1002a3bad5063e
Giovanni Muraca <ghadprogrammer@gmail.com>
2024-12-10 21:23:35 +0100

    Tolte robe che mi ha chiesto Tonino

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './layout/header/header.component';
-import {FooterComponent} from './layout/footer/footer.component';
-import {MapComponent} from './map/map.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MapComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
