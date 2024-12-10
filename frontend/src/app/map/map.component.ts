527db5bb8c5f2e963743623a773fd804e06a0cb4
Giovanni Muraca <ghadprogrammer@gmail.com>
2024-12-10 21:26:30 +0100

    Aggiunto componente mappa, aggiunto il componente alla home.

--- frontend/src/app/map/map.component.ts
+++ frontend/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrl: './map.component.css'
 })
-export class MapComponent {
 
+export class MapComponent implements OnInit {
+  private map: L.Map | undefined;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    // Crea la mappa
+    this.map = L.map('map', {
+      center: [39.35661,16.2406215], // Latitudine e longitudine iniziali
+      zoom: 16,
+      zoomControl: false
+    });
+
+    // Aggiungi un livello tile (es. OpenStreetMap)
+    L.tileLayer('https://api.maptiler.com/maps/bright-v2/{z}/{x}/{y}.png?key=3Hge8kPqBdsue4YlJEmA', {
+      maxZoom: 16,
+      attribution: 'GesTonino',
+      minZoom: 16,
+    }).addTo(this.map);
+  }
 }
