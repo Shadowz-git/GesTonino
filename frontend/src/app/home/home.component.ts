b717b2be7c96939b3121c5c0476c1fad508db5b2
Shad <sh4dowswat@gmail.com>
2025-01-07 18:27:55 +0100

    Cambiato Login

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../layout/header/header.component';
 import {MapComponent} from '../map/map.component';
+import {LoginComponent} from '../auth/login/login.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeaderComponent,
-    MapComponent
+    MapComponent,
+    LoginComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
