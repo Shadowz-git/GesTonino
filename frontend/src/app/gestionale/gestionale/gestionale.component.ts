470159156dfef2f854591bbe586fb7018cf58a8c
Shad <sh4dowswat@gmail.com>
2025-01-07 18:18:18 +0100

    Aggiunti componenti e Material Package per Angular

--- frontend/src/app/gestionale/gestionale/gestionale.component.ts
+++ frontend/src/app/gestionale/gestionale/gestionale.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {NavbarComponent} from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-gestionale',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavbarComponent
   ],
   templateUrl: './gestionale.component.html',
   styleUrl: './gestionale.component.css'
