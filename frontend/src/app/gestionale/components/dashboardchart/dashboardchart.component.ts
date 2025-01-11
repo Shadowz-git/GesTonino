96fa4aaefad3a0bb80bc31ec3206024229637b7a
Giovanni Muraca <ghadprogrammer@gmail.com>
2025-01-11 12:47:07 +0100

    Proseguono i lavori sul design della dashboard, creato componente per i grafici perla ma donna

---
+++ frontend/src/app/gestionale/components/dashboardchart/dashboardchart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardchart',
+  imports: [],
+  templateUrl: './dashboardchart.component.html',
+  styleUrl: './dashboardchart.component.css'
+})
+export class DashboardchartComponent {
+
+}
--- frontend/src/app/gestionale/dashboard/dashboard.component.ts
+++ frontend/src/app/gestionale/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {DashboardchartComponent} from '../components/dashboardchart/dashboardchart.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
+    DashboardchartComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
