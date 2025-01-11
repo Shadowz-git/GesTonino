561bc15a788c384b74133466960c504eadfb7037
Shad <sh4dowswat@gmail.com>
2025-01-11 17:07:18 +0100

    Ridotto codice doppio

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -6,18 +6,18 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private loginUril = 'http://localhost:8080/api/auth/login'; // Modifica l'URL se necessario
-  private registerUrl = 'http://localhost:8080/api/auth/register'; // Modifica l'URL se necessario
+  private apiUrl = 'http://localhost:8080/api/auth';
+
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<boolean> {
     const payload = { email, password };
-    return this.http.post<boolean>(this.loginUril, payload);
+    return this.http.post<boolean>(`${this.apiUrl}/login`, payload);
   }
 
   register(email: string, password: string): Observable<boolean> {
     const payload = { email, password };
-    return this.http.post<boolean>(this.registerUrl, payload);
+    return this.http.post<boolean>(`${this.apiUrl}/register`, payload);
   }
 }
