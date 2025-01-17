8242beb93c0d29d42684c86f51a7e46f42459e06
Giovanni Muraca <ghadprogrammer@gmail.com>
2025-01-17 16:49:31 +0100

    Aggiunta api per salvare la nuova attività nel db

--- backend/src/main/java/com/gestonino/backend/controllers/ActivityController.java
+++ backend/src/main/java/com/gestonino/backend/controllers/ActivityController.java
@@ -2,10 +2,11 @@ package com.gestonino.backend.controllers;
 
 import com.gestonino.backend.model.dao.ActivityRepository;
 import com.gestonino.backend.model.types.Activity;
+import com.gestonino.backend.model.types.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +20,12 @@ public class ActivityController {
     @GetMapping
     public List<Activity> getAllActivities() {
         return activityRepository.findAll();
+    }
+
+    @PostMapping("/createActivity")
+    public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
+        System.out.println("Qua Activity");
+        Activity activitySaved= activityRepository.save(activity);
+        return new ResponseEntity<>(activitySaved, HttpStatus.CREATED);
     }
 }
