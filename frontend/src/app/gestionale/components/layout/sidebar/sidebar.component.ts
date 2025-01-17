1e4914ce2b208aaf8242b0f33dd881f2ac782cdd
Giovanni Muraca <ghadprogrammer@gmail.com>
2025-01-17 16:43:12 +0100

    Sistemato tasto sidebar in modo che apra il popup per le impostazioni

--- frontend/src/app/gestionale/components/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/gestionale/components/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,29 @@
 import {Component} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
+import {SettingsDialogComponent} from '../../settings-dialog/settings-dialog.component';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    SettingsDialogComponent,
+    NgIf
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 
 export class SidebarComponent {
+  isSettingsOpen = false;
+
+  openSettings() {
+    this.isSettingsOpen = true;
+  }
+
+  closeSettings() {
+    this.isSettingsOpen = false;
+  }
+
 }
